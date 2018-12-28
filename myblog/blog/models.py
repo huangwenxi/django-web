e134d3642c8016724a296da111e0e1d283dec51c
huangwenxi <389125903@qq.com>
2018-12-28 15:08:06 +0800

    1:增加了models的使用 2:配置了增加了管理员用户

--- myblog/blog/models.py
+++ myblog/blog/models.py
@@ -3,4 +3,10 @@ from __future__ import unicode_literals
 
 from django.db import models
 
-# Create your models here.
+
+class Article(models.Model):
+    title = models.CharField(max_length=32, default='Title')
+    content = models.TextField(max_length=32, default='Content')
+
+    def __str__(self):
+        return self.title
--- myblog/blog/views.py
+++ myblog/blog/views.py
@@ -3,6 +3,8 @@ from __future__ import unicode_literals
 
 from django.shortcuts import render
 from django.http import HttpResponse
+from blog.models import Article
+
 
 def index(request):
     return render(request, 'blog/index.html', {'hello':'hello blog'})
